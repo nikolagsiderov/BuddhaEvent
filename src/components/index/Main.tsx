622f5dc53d7f9740c1ef647ae50f7f6c323f4006
nikolagsiderov <nikolagsiderov@gmail.com>
2024-02-01 19:48:06 +0200

    Further animations...

--- src/components/index/Main.tsx
+++ src/components/index/Main.tsx
@@ -12,48 +12,58 @@ export default function Hero() {
     countdownSeconds: 0,
   });
 
-  const countdownTimer = () => {
-    const timeInterval = setInterval(() => {
-      const countdownDateTime = new Date(expiryTime).getTime();
-      const currentTime = new Date().getTime();
-      const remainingDayTime = countdownDateTime - currentTime;
-      const totalDays = Math.floor(remainingDayTime / (1000 * 60 * 60 * 24));
-      const totalHours = Math.floor(
-        (remainingDayTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      const totalMinutes = Math.floor(
-        (remainingDayTime % (1000 * 60 * 60)) / (1000 * 60)
-      );
-      const totalSeconds = Math.floor((remainingDayTime % (1000 * 60)) / 1000);
-
-      const runningCountdownTime = {
-        countdownDays: totalDays,
-        countdownHours: totalHours,
-        countdownMinutes: totalMinutes,
-        countdownSeconds: totalSeconds,
-      };
-
-      setCountdownTime(runningCountdownTime);
-
-      if (remainingDayTime < 0) {
-        clearInterval(timeInterval);
-        setExpiryTime("");
-      }
-    }, 1000);
-  };
-
   useEffect(() => {
-    countdownTimer();
-  });
+    const countdownTimer = () => {
+      const timeInterval = setInterval(() => {
+        const countdownDateTime = new Date(expiryTime).getTime();
+        const currentTime = new Date().getTime();
+        const remainingDayTime = countdownDateTime - currentTime;
+        if (remainingDayTime < 0) {
+          clearInterval(timeInterval);
+          setExpiryTime("");
+          setCountdownTime({
+            countdownDays: 0,
+            countdownHours: 0,
+            countdownMinutes: 0,
+            countdownSeconds: 0,
+          });
+          return;
+        }
+
+        const totalDays = Math.floor(remainingDayTime / (1000 * 60 * 60 * 24));
+        const totalHours = Math.floor(
+          (remainingDayTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        const totalMinutes = Math.floor(
+          (remainingDayTime % (1000 * 60 * 60)) / (1000 * 60)
+        );
+        const totalSeconds = Math.floor(
+          (remainingDayTime % (1000 * 60)) / 1000
+        );
+
+        setCountdownTime({
+          countdownDays: totalDays,
+          countdownHours: totalHours,
+          countdownMinutes: totalMinutes,
+          countdownSeconds: totalSeconds,
+        });
+      }, 1000);
+
+      return () => clearInterval(timeInterval); // Clear interval on component unmount
+    };
+
+    const timer = countdownTimer();
+    return () => clearInterval(timer); // Ensure this is returned to clear the interval when the component unmounts or the expiryTime changes
+  }, [expiryTime]);
 
   return (
     <div>
-      <div className="mb-6 sticky top-[7rem] lg:mt-0">
+      <div className="top-[7rem]">
         <h1 className="text-3xl lg:text-3xl leading-tight lg:leading-tight font-black">
           <motion.span
             initial="hidden"
             whileInView="visible"
-            transition={{ duration: 0.1, delay: 0.2 }}
+            transition={{ duration: 0.1, delay: 1.2 }}
             viewport={{ once: true }}
             variants={{
               visible: { opacity: 1, y: 0, scale: 1 },
@@ -65,7 +75,7 @@ export default function Hero() {
           <motion.span
             initial="hidden"
             whileInView="visible"
-            transition={{ duration: 0.1, delay: 0.4 }}
+            transition={{ duration: 0.1, delay: 1.4 }}
             viewport={{ once: true }}
             variants={{
               visible: { opacity: 1, y: 0, scale: 1 },
@@ -77,7 +87,7 @@ export default function Hero() {
           <motion.span
             initial="hidden"
             whileInView="visible"
-            transition={{ duration: 0.1, delay: 1 }}
+            transition={{ duration: 0.1, delay: 2 }}
             viewport={{ once: true }}
             variants={{
               visible: { opacity: 1, y: 0, scale: 1 },
@@ -88,20 +98,11 @@ export default function Hero() {
             OF
           </motion.span>
           <br />
-          <motion.span
-            initial="hidden"
-            whileInView="visible"
-            transition={{ duration: 0.1, delay: 0.6 }}
-            viewport={{ once: true }}
-            variants={{
-              visible: { opacity: 1, y: 0, scale: 1 },
-              hidden: { opacity: 0, y: 0, scale: 0.9 },
-            }}
-          >
+          <span className="text-white text-5xl animate-[pulse_0.9s_ease-in-out_infinite] animate-ping">
             PULSE
-          </motion.span>
+          </span>
         </h1>
-        <div className="lg:flex gap-6 lg:gap-16 items-center justify-center my-8 lg:pb-24">
+        <div className="lg:flex items-center justify-center my-8 lg:pb-24">
           <motion.div
             initial="hidden"
             whileInView="visible"
@@ -182,29 +183,30 @@ export default function Hero() {
         </div>
         <motion.div
           initial="hidden"
-          whileInView="visible"
+          animate="visible"
           transition={{ duration: 0.2, delay: 3 }}
           viewport={{ once: true }}
           variants={{
             visible: { opacity: 1, y: 0, scale: 1 },
             hidden: { opacity: 0, y: 0, scale: 0.5 },
           }}
-          className="lg:flex items-center justify-center"
+          className="flex items-center justify-center"
         >
           <h1 className="text-3xl lg:text-9xl lg:leading-tight font-black">
             <span className="bg-gradient-to-r from-white/40 to-moon/90 text-transparent bg-clip-text">
               <TypeAnimation
                 sequence={[
-                  // Same substring at the start will only be typed out once, initially
                   "COMING SOON",
-                  1000, // wait 1s before replacing "Mice" with "Hamsters"
+                  1000,
                   "16 - 19 MARCH",
                   1000,
                   "96 HOURS",
                   1000,
+                  "COMING SOON",
+                  1000,
                 ]}
                 wrapper="span"
-                speed={50}
+                speed={60}
                 style={{ display: "inline-block" }}
                 repeat={Infinity}
               />
