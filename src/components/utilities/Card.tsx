77648cde71972bd444a4db57d5f60e8acf1cd103
nikolagsiderov <nikolagsiderov@gmail.com>
2024-02-07 12:41:13 +0200

    Change of background...

--- src/components/utilities/Card.tsx
+++ src/components/utilities/Card.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({ children }: { children: JSX.Element[] }) => {
   return (
-    <div className="lg:w-[50%] rounded-[20px] justify-center items-center text-center bg-moon/20 backdrop-blur mx-24 mt-6 p-4 lg:p-6 lg:mb-24">
+    <div className="lg:w-[50%] rounded-[20px] justify-center items-center text-center bg-black/20 backdrop-blur mx-24 mt-6 p-4 lg:p-6 lg:mb-24">
       {children}
     </div>
   );
